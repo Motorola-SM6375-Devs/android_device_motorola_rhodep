a4f51a8153dd429fb97d047ad0d6fc7de5d216be
Anand S <anandzzz360@gmail.com>
2025-10-23 12:30:52 +0530

    rhodep: Add common libqti-perfd-client to namespaces
    * Needed by libets_teeclient_v2.so
    
    Change-Id: I66ca16099ae47b61be980ceacb604ccddeec95b6

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,7 @@ from extract_utils.main import (
 namespace_imports = [
     'device/motorola/rhodep',
     'hardware/motorola',
+    'hardware/qcom-caf/common/libqti-perfd-client',
     'vendor/motorola/sm6375-common',
     'vendor/qcom/opensource/display',
 ]
